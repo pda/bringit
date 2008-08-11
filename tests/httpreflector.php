f543a4bae30967d6fb563a3afe7cb7f85942c42e
Paul Annesley <paul@annesley.cc>
2008-08-12 01:46:04 +1000

    Improved activity console output in httpreflector.

--- tests/httpreflector.php
+++ tests/httpreflector.php
@@ -19,29 +19,38 @@ while (true)
 {
 	if ($conn = stream_socket_accept($socket, -1, $peername))
 	{
-		echo "Accepted connection from $peername\n";
+		echo "\nAccepted connection from $peername\n";
 
-		$data = '';
+		$data = fgets($conn);
+		printf("  %s\n", trim($data));
 
 		// read headers
 		while (($data .= fread($conn, 1)) !== false && substr($data, -4, 4) !== "\r\n\r\n");
 
 		// read body up to content-length if specified
 		if (preg_match('#\r\nContent-Length: (\d+)\r\n#i', $data, $matches))
+		{
+			printf("  Expecting %d byte body\n", $matches[1]);
+
 			for ($i = 0; $i < $matches[1]; $i++)
 				if (($char = fread($conn, 1)) !== false)
 					$data .= $char;
+		}
+
+		$requestDataLength = strlen($data);
+		printf("  Reflecting %d bytes of raw request data... ", $requestDataLength);
 
 		// write response, using the entire raw request as the response body
 		fwrite($conn,
 			"HTTP/1.0 200 OK\r\n".
 			"Content-Type: text/plain\r\n".
-			"Content-Length: ".strlen($data)."\r\n".
+			"Content-Length: $requestDataLength\r\n".
 			"\r\n".
 			$data
 		);
 
 		fclose($conn);
+		echo "done\n";
 	}
 }
 
